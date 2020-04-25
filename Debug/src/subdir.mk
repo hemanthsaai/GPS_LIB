4f45fec75a40201e7bffb00672a4c857b4ecd8ab
Hemanthsai <hemanth.saai@gmail.com>
2020-04-25 21:28:29 +0530

    Eclipse change

---
+++ Debug/src/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/Library.c \
+../src/gps_lib.c \
+../src/he_std.c 
+
+OBJS += \
+./src/Library.o \
+./src/gps_lib.o \
+./src/he_std.o 
+
+C_DEPS += \
+./src/Library.d \
+./src/gps_lib.d \
+./src/he_std.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cygwin C Compiler'
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
